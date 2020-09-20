2da15c5962b3a174181b9823104e5689e88877c2
ThereNoTeaThisAfternoon <Q13056622931@outlook.com>
2020-09-20 20:19:56 +0800

    左叶子之和

--- 树/src/左叶子之和/SumOfLeftLeaves.java
+++ 树/src/左叶子之和/SumOfLeftLeaves.java
@@ -6,7 +6,6 @@ import Tree公共方法.TreeNode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
