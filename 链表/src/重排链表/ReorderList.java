27c42e04c2fa45167e39dae965d566c396e072f8
ThereIsNoTeaThisAfternoon <1622410986@qq.com>
2020-10-20 20:51:46 +0800

    ReorderList 2020.10.20 - 下午 8:05

--- 链表/src/重排链表/ReorderList.java
+++ 链表/src/重排链表/ReorderList.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
