6549451d0df872f325bd23197640cd5ce1524ece
ThereIsNoTeaThisAfternoon <1622410986@qq.com>
2020-11-08 18:20:32 +0800

    create

--- 贪心/src/Greedy公共方法/PublicMethod.java
+++ 贪心/src/Greedy公共方法/PublicMethod.java
@@ -8,6 +8,20 @@ package Greedy公共方法;
  */
 public class PublicMethod {
 
-
-
+    /**
+     * String:"[7,1,5,3,6,4]" -> int[]
+     */
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim(); // 去除两旁多余空格
+        input = input.substring(1, input.length() - 1); // 去除两旁括号
+        if ("".equals(input)) {
+            return new int[0];
+        }
+        String[] nums = input.split(",");
+        int[] output = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = Integer.parseInt(nums[i]);
+        }
+        return output;
+    }
 }
