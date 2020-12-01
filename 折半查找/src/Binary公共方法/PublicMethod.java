59b163884445de552f9835c98fdcb0c611a7cb57
ThereIsNoTeaThisAfternoon <1622410986@qq.com>
2020-12-01 21:26:02 +0800

    update

--- 折半查找/src/Binary公共方法/PublicMethod.java
+++ 折半查找/src/Binary公共方法/PublicMethod.java
@@ -15,30 +15,56 @@ public class PublicMethod {
         return output;
     }
 
-    //string -> IntegerArray  [[1,5,9],[10,11,13],[12,13,15]]
+    /**
+     * String:"[[1,5,9],[10,11,13],[12,13,15]]" -> int[][]
+     */
     public static int[][] stringTo2DIntegerArray(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0)
+        input = input.trim(); // 去除两旁多余空格
+        input = input.substring(1, input.length() - 1); // 去掉左右括号
+        if (input.length() == 0) {
             return new int[0][];
-        int n = 0;
-        for (char ch : input.toCharArray())
-            if (ch == ']')
-                n++;
-        int[][] out = new int[n][n];
+        }
+        // 获取行数、列数
+        int rows = 0, cols = 1;
+        boolean button = true;
+        for (char ch : input.toCharArray()) {
+            if (ch == ']') {
+                rows++;
+                button = false;
+            } else if (button && ch == ',') {
+                cols += 1;
+            }
+        }
+        int[][] out = new int[rows][cols];
         int i = 0;
         StringBuilder str = new StringBuilder();
-        for (int a = 0; a < n; a++) {
-            for (int b = 0; b < n; b++) {
+        for (int a = 0; a < rows; a++) {
+            for (int b = 0; b < cols; b++) {
                 while (i < input.length() && !Character.isDigit(input.charAt(i)))
                     i++;
                 while (i < input.length() && Character.isDigit(input.charAt(i))) {
                     str.append(input.charAt(i++));
                 }
-                out[a][b] = Integer.parseInt(str.toString().toString());
+                out[a][b] = Integer.parseInt(str.toString());
                 str = new StringBuilder();
             }
         }
         return out;
     }
+
+    /**
+     * int[][]:{{1,2},{1,2}} -> String
+     */
+    public static String integer2DArrayToString(int[][] input) {
+        StringBuilder output = new StringBuilder("[");
+        for (int[] arr : input) {
+            StringBuilder sb = new StringBuilder("[");
+            for (int elem : arr) {
+                sb.append(elem).append(",");
+            }
+            output.append(sb.substring(0, sb.length() - 1)).append("], ");
+        }
+        return output.substring(0, output.length() - 2) + "]";
+    }
+
 }
