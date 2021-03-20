e99bb964a4616324431f9defb271f0836525cbbf
ThereIsNoTeaThisAfternoon <1622410986@qq.com>
2021-03-20 14:09:55 +0800

    update 2021.3.20 - 下午 12:50

--- 栈/src/Stack公共方法/PublicMethod.java
+++ 栈/src/Stack公共方法/PublicMethod.java
@@ -14,4 +14,21 @@ public class PublicMethod {
             output[index++] = Integer.parseInt(part);
         return output;
     }
+
+    /**
+     * @param input ["2","1","+","3","*"]
+     * @return output String[]{2,1,+,3,*}
+     */
+    public static String[] stringToStringArray(String input) {
+        input = input.trim(); // 去除两旁多于空格
+        input = input.substring(1, input.length() - 1); // 去除两旁中括号
+        if (input.length() < 1) {
+            return new String[0];
+        }
+        String[] output = input.split(",");
+        for (int i = 0; i < output.length; ++i) {
+            output[i] = output[i].substring(1, output[i].length() - 1);
+        }
+        return output;
+    }
 }
